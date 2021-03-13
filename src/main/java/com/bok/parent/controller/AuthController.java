480034601201da291bfa6489a05731d4f86c75da
cfaraone <cfaraone@soldo.com>
2021-03-13 14:19:40 +0100

    continued implementation

--- src/main/java/com/bok/parent/controller/AuthController.java
+++ src/main/java/com/bok/parent/controller/AuthController.java
@@ -1,23 +1,22 @@
 package com.bok.parent.controller;
 
 import com.bok.parent.config.TotpManager;
-import com.bok.parent.service.UserService;
 import com.bok.parent.dto.*;
 import com.bok.parent.exception.BadRequestException;
 import com.bok.parent.exception.EmailAlreadyExistsException;
 import com.bok.parent.exception.UsernameAlreadyExistsException;
 import com.bok.parent.model.User;
+import com.bok.parent.service.UserService;
+import com.bok.parent.util.Constants;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.StringUtils;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import com.bok.parent.util.Constants;
 
 import java.net.URI;
 
@@ -35,13 +34,13 @@ public class AuthController {
     public JwtAuthenticationResponse authenticateUser(@RequestBody LoginRequest loginRequest) {
         log.debug("user {} attempting login", loginRequest.username);
         String token = userService.loginUser(loginRequest.username, loginRequest.password);
-        return new JwtAuthenticationResponse(token, StringUtils.isEmpty(token));
+        return new JwtAuthenticationResponse(token, ObjectUtils.isEmpty(token));
     }
 
     @PostMapping("/verify")
     public JwtAuthenticationResponse verifyCode(@RequestBody VerifyCodeRequest verifyCodeRequest) {
         String token = userService.verify(verifyCodeRequest.username, verifyCodeRequest.code);
-        return new JwtAuthenticationResponse(token, StringUtils.isEmpty(token));
+        return new JwtAuthenticationResponse(token, ObjectUtils.isEmpty(token));
     }
 
     @PostMapping(value = "/register", produces = MediaType.APPLICATION_JSON_VALUE)
