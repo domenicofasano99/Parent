baa776ab3fd386c529594242b12c9ae1d31de911
cfaraone <cfaraone@soldo.com>
2021-03-14 16:17:41 +0100

    added queue for user creation messages

---
+++ src/main/java/com/bok/parent/messaging/UserCreationMessageProducer.java
@@ -0,0 +1,31 @@
+package com.bok.parent.messaging;
+
+import com.bok.integration.UserCreationDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class UserCreationMessageProducer {
+
+    @Autowired
+    JmsTemplate jmsTemplate;
+
+    @Value("${active-mq.transfers-queue}")
+    private String transfersTopic;
+
+
+    public void send(UserCreationDTO userCreationDTO) {
+        try {
+            log.info("Attempting Send transfer to Topic: " + transfersTopic);
+            jmsTemplate.convertAndSend(transfersTopic, userCreationDTO);
+        } catch (Exception e) {
+            log.error("Received Exception during send Message: ", e);
+        }
+    }
+
+
+}
