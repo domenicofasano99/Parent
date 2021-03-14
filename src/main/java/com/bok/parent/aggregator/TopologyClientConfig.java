2edaf24c7e35760cf1ca230cf97c8806715fc5b8
cfaraone <cfaraone@soldo.com>
2021-03-14 15:16:00 +0100

    fixed tests and application.yml

--- src/main/java/com/bok/parent/aggregator/TopologyClientConfig.java
+++ src/main/java/com/bok/parent/aggregator/TopologyClientConfig.java
@@ -1,21 +1,16 @@
 package com.bok.parent.aggregator;
 
-import feign.Feign;
-import feign.Logger;
-import feign.slf4j.Slf4jLogger;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableFeignClients
 public class TopologyClientConfig {
 
-    @Value("${topology.krypto.hostname}")
-    private String kryptoHostname;
+    //@Value("${topology.krypto.hostname}")
+    //private String kryptoHostname;
 
-    @Value("${topology.krypto.port}")
-    private Integer kryptoPort;
+    //@Value("${topology.krypto.port}")
+    //private Integer kryptoPort;
 
 }
