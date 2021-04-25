a3942e52c33da826a9b8fdd9c3b46a517c1bf3ba
fearlessfara <christiangenn99gmail.com>
2021-04-25 02:14:27 +0200

    fixed application.yml

--- parent-be/src/test/java/com/bok/parent/ParentApplicationTests.java
+++ parent-be/src/test/java/com/bok/parent/ParentApplicationTests.java
@@ -12,9 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.Assert.assertEquals;
