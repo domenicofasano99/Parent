09c9eef961e3c91ee312f04b6bb6aa82a12550d4
cfaraone <cfaraone@soldo.com>
2021-05-16 17:11:47 +0200

    changed a method and temporarly removed caching

--- parent-be/src/main/java/com/bok/parent/helper/SecurityHelper.java
+++ parent-be/src/main/java/com/bok/parent/helper/SecurityHelper.java
@@ -42,16 +42,6 @@ public class SecurityHelper {
         return response;
     }
 
-    /***
-     * Cache is used here to speedup the retrieval process, since this bean is not accessible from the outside
-     * it's considered a safe process to do.
-     * Extracting the userId from the token is an expensive operation since a query is needed to do so.
-     * Caching the result will improve the speed by a lot since there will be probably multiple requests from the same
-     * user in a defined period of time.
-     * @param token the token that arrives from the request sent by the client
-     * @return the ID of the account that has made the request
-     */
-    @Cacheable(value = Constants.TOKENS, unless = "#result == null")
     public Long extractAccountId(String token) {
         return jwtAuthenticationHelper.extractAccountIdFromToken(token);
     }
