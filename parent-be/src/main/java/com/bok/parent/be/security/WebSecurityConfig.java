6477f499620009c6cfcfd1e5f44c3b8e6c68d371
cfaraone <cfaraone@soldo.com>
2021-06-09 02:03:58 +0200

    added verify endpoint to security exceptions

--- parent-be/src/main/java/com/bok/parent/be/security/WebSecurityConfig.java
+++ parent-be/src/main/java/com/bok/parent/be/security/WebSecurityConfig.java
@@ -56,7 +56,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         // We don't need CSRF for this example
         httpSecurity
                 .authorizeRequests()
-                .antMatchers("/login", "/register")
+                .antMatchers("/login", "/register", "/verify")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
