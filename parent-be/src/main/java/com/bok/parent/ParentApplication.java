054ec95e0e9be39bb385bbdf6f9ad36fbd289840
cfaraone <cfaraone@soldo.com>
2021-06-13 19:09:15 +0200

    minor fixes

--- parent-be/src/main/java/com/bok/parent/ParentApplication.java
+++ parent-be/src/main/java/com/bok/parent/ParentApplication.java
@@ -2,12 +2,10 @@ package com.bok.parent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
-@EnableEurekaServer
 public class ParentApplication {
     public static void main(String[] args) {
         SpringApplication.run(ParentApplication.class, args);
