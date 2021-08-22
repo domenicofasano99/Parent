b32dee2dd804da7de81aae63324be670934fb004
cfaraone <christiangennaro.faraone@mail.polimi.it>
2021-08-22 16:26:38 +0200

    minor fixes

--- parent-be/src/test/java/com/bok/parent/AccountServiceTest.java
+++ parent-be/src/test/java/com/bok/parent/AccountServiceTest.java
@@ -18,10 +18,12 @@ import com.github.javafaker.Faker;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.time.ZonedDateTime;
 import java.util.Date;
